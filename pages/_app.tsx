f7d16a30b4b6b5c9608e1c0bfebf72eb8d9f5429
Emerson Vieira <emerson.chaves@nuvem.net>
2025-02-01 17:45:49 -0400

    chore: implementa o HeaderMenu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { HeaderMenu } from './_components/HeaderMenu'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <HeaderMenu />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
---
+++ pages/_components/HeaderMenu.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export function HeaderMenu() {
+  const router = useRouter();
+
+  return (
+    <div className="py-3 flex align-middle justify-center gap-6 border-b-1 border-gray-300">
+      <Link
+        href={"/"}
+        className={
+          `${router.pathname === '/' ? "bg-blue-500" : "bg-white border-1"} px-6 py-1 rounded-md`
+        }
+      >
+        <span className={router.pathname === '/' ? "text-white" : "text-black"}>
+          Posts
+        </span>
+      </Link>
+      <Link
+        href={"/admin"}
+        className={
+          `${router.pathname === '/admin' ? "bg-blue-500" : "bg-white border-1"} px-6 py-1 rounded-md`
+        }
+      >
+        <span className={router.pathname === '/admin' ? "text-white" : "text-black"}>
+          Admin
+        </span>
+      </Link>
+    </div>
+  );
+}
